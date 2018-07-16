4c6952f7848684563fe6da912885b50716092eeb
Christine Xiang <yutingcxiang@gmail.com>
2018-07-16 04:10:21 +0000

    automatically backed up by learn

---
+++ lib/genre.rb
@@ -0,0 +1,20 @@
+class Genre
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_song(name, artist)
+    Song.new(name, artist)
+  end
+  
+  def artists
+  end
+end
