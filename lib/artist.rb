62ec50535ceb62bf76642a38aa121095ce87431e
Christine Xiang <yutingcxiang@gmail.com>
2018-07-16 04:15:23 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,8 +11,8 @@ class Artist
     @@all
   end
   
-  def new_song(name, artist, genre)
-    Song.new(self, name, artist, genre)
+  def new_song(name, genre)
+    Song.new(name, self, genre)
   end
   
   def songs
