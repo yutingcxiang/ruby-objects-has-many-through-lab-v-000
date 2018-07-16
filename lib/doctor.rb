e2397837e974d60753e261aca01f2afbd10871c2
Christine Xiang <yutingcxiang@gmail.com>
2018-07-16 04:35:36 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -2,9 +2,21 @@ class Doctor
   
   @@all = []
   
+  def 
+  
+  
   def self.all
     @@all
   end
   
+  def new_appointment(date, patient)
+    
+  end
+  
+  def appointments
+  end
+  
+  def patients
+  end
   
 end
